a6deb0e53e3fc47e7098a7b96f6cd854a89f0f82
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-22 15:45:43 +0800

    feat(repository): add findByEmail method to AdminRepository

--- src/main/java/com/example/demo/repositories/AdminRepository.java
+++ src/main/java/com/example/demo/repositories/AdminRepository.java
@@ -4,8 +4,10 @@ import com.example.demo.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 // The type after the model is the primary key
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
     List<Admin> findByIsActiveTrue();
+    Optional<Admin> findByEmail(String email);
 }
