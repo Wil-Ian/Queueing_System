0d18b4bc209fc562697bc0db1981dbcaf697f84b
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-22 16:19:26 +0800

    added Password to Person

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -6,6 +6,7 @@ import jakarta.persistence.MappedSuperclass;
 public class Person {
     private String name;
     private String email;
+    private String password;
 
     public String getName() {
         return name;
@@ -21,5 +22,13 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
