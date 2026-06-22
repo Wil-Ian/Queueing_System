8101d97221a66ea52e89dc5c5d6f8756d2605557
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-22 15:49:59 +0800

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// spring.jpa.open-in-view is enabled during development phase and can be
+    // spring.jpa.open-in-view is enabled during development phase and can be
 	// turned off after deployment to improve performance
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
