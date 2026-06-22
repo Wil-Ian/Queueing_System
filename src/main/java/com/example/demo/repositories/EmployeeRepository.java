8614ec2149fca5e48e7002890371fedb72b6c45d
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-22 15:51:01 +0800

    feat(repository): add findByEmail method to EmployeeRepository

--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -4,8 +4,10 @@ import com.example.demo.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 // The type after the model is the primary key
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findByIsActiveTrue();
+    Optional<Employee> findByEmail(String email);
 }
