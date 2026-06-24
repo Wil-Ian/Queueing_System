8a9d0d00ddbc53373ca05c23c92ad3dae3a0ce5d
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-24 13:32:37 +0800

    implemented logout endpoint

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -3,10 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.dto.LoginRequest;
 import com.example.demo.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,5 +18,15 @@ public class AuthController {
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request) {
         return authService.login(request.getEmail(), request.getPassword());
+    }
+
+    @PostMapping("/logout")
+    public String logout(@RequestHeader("Authorization") String authHeader) {
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            authService.logout(token);
+            return "Logged out successfully.";
+        }
+        return "Error with header.";
     }
 }
