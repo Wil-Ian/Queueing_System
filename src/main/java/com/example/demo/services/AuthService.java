3e7f367c0df1aba0b62b4f99f7f0d0d16560b32c
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-26 08:53:27 +0800

    Implemented refresh feature to authService

--- src/main/java/com/example/demo/services/AuthService.java
+++ src/main/java/com/example/demo/services/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.AuthResponse;
 import com.example.demo.exceptions.InvalidCredentialsException;
 import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.models.Admin;
@@ -32,25 +33,48 @@ public class AuthService {
         this.blacklistedTokensRepository = blacklistedTokensRepository;
     }
 
-    public String login(String email, String password) {
+    public AuthResponse login(String email, String password) {
         Optional<Employee> existingEmployee = employeeRepository.findByEmail(email);
         Optional<Admin> existingAdmin = adminRepository.findByEmail(email);
         if(existingEmployee.isPresent()) {
             Employee employee = existingEmployee.get();
             if(passwordEncoder.matches(password, employee.getPassword())) {
-                return util.generateToken(email, "EMPLOYEE");
+                AuthResponse authResponse = new AuthResponse();
+                authResponse.setAccessToken(util.generateToken(email, "EMPLOYEE"));
+                authResponse.setRefreshToken(util.generateRefreshToken(email));
+                return authResponse;
             } else {
                 throw new InvalidCredentialsException("Invalid password.");
             }
         } else if(existingAdmin.isPresent()) {
             Admin admin = existingAdmin.get();
             if(passwordEncoder.matches(password, admin.getPassword())) {
-                return util.generateToken(email, "ADMIN");
+                AuthResponse authResponse = new AuthResponse();
+                authResponse.setAccessToken(util.generateToken(email, "ADMIN"));
+                authResponse.setRefreshToken(util.generateRefreshToken(email));
+                return authResponse;
             } else {
                 throw new InvalidCredentialsException("Invalid password.");
             }
         } else {
             throw new ResourceNotFoundException("No account found with this email.");
+        }
+    }
+
+    public AuthResponse refresh(String refreshToken) {
+        String email = util.extractEmail(refreshToken);
+        Optional<Employee> existingEmployee = employeeRepository.findByEmail(email);
+        Optional<Admin> existingAdmin = adminRepository.findByEmail(email);
+        if(existingEmployee.isPresent()) {
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setAccessToken(util.generateToken(email, "EMPLOYEE"));
+            return authResponse;
+        } else if(existingAdmin.isPresent()) {
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setAccessToken(util.generateToken(email, "ADMIN"));
+            return authResponse;
+        } else {
+            throw new ResourceNotFoundException("No user with that email exists in our database.");
         }
     }
 
