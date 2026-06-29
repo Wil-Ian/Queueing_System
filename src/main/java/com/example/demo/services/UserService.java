2bf5f2b65c2ea89aad541d202221deb4e575ffcb
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-29 14:11:29 +0800

    Initialize active flag and save new user

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -6,6 +6,7 @@ import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,8 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        user.setTimeStamp(LocalDateTime.now());
+        user.setActive(true);
         return userRepository.save(user);
     }
 
