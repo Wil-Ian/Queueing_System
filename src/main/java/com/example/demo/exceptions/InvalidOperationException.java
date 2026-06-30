f7f3f770e6518ec3dd7fafb12367fa25d2168ab2
Wil-Ian <wilianmanuntag@gmail.com>
2026-06-30 14:08:11 +0800

    Add error code field, constructor, and getter to InvalidOperationException

--- src/main/java/com/example/demo/exceptions/InvalidOperationException.java
+++ src/main/java/com/example/demo/exceptions/InvalidOperationException.java
@@ -1,7 +1,17 @@
 package com.example.demo.exceptions;
 
 public class InvalidOperationException extends RuntimeException{
+    private String code;
     public InvalidOperationException(String message) {
         super(message);
+    }
+
+    public InvalidOperationException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
