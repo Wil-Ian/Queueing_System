af2fd74a5714d34b7392da23ff5d9e0bf67fb87a
Wil-Ian <wilianmanuntag@gmail.com>
2026-07-03 16:53:42 +0800

    optimize dashboard loading using Promise.all and implement requeue action

--- src/main/resources/templates/receivingDashboard.js
+++ src/main/resources/templates/receivingDashboard.js
@@ -1,4 +1,5 @@
 const token = "";
+let currentServingId = null;
 
 function loadDashboard() {
     fetch("https://localhost:8443/employee/me", {
@@ -20,33 +21,58 @@ function loadDashboard() {
                 windowTitle.innerHTML = employee.window.category;
             }
 
-            return fetch(`https://localhost:8443/queue/window-queue?windowId=${windowId}`, {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Content-Type": "application/json"
-                }
-            });
-        })
-        .then(response => {
-            return response.json().then(body => {
-                if (!response.ok) {
-                    const err = new Error(body.error);
-                    err.code = body.code;
-                    throw err;
-                }
-                return body;
-            });
-        })
-        .then(queues => {
-            const queueTable = document.getElementById("queueTable");
+            const mainFetch = Promise.all([
+                fetch(`https://localhost:8443/queue/window-queue?windowId=${windowId}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }),
+                fetch(`https://localhost:8443/queue/reports/queue-count?windowId=${windowId}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }),
+                fetch(`https://localhost:8443/queue/reports/missed-count?windowId=${windowId}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }),
+                fetch(`https://localhost:8443/queue/reports/daily-volume?windowId=${windowId}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }),
+                fetch(`https://localhost:8443/queue/reports/transfer-count?windowId=${windowId}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                })
+            ]).then(responses => {
+                return Promise.all(responses.map(response => response.json()));
+            })
+                .then(([queues, queueCount, missedCount, dailyVolume, transferCount]) => {
+                    const queueTable = document.getElementById("queueTable");
+                    const totalQueue = document.getElementById("totalQueue");
+                    const totalMissed = document.getElementById("totalMissed");
+                    const totalComplete = document.getElementById("totalComplete");
+                    const totalReferred = document.getElementById("totalReferred");
 
-            if (queueTable) {
-                queueTable.innerHTML = "";
-
-                queues.forEach(queue => {
-                    const row = document.createElement("tr");
-                    row.innerHTML = `
+                    // queue table
+                    if (queueTable) {
+                        queueTable.innerHTML = "";
+                        queues.forEach(queue => {
+                            const row = document.createElement("tr");
+                            row.innerHTML = `
                     <td>${queue.user.name}</td>
                     <td>${queue.timeStamp}</td>
                     <td>${queue.user.priority}</td>
@@ -54,15 +80,72 @@ function loadDashboard() {
                     <td>
                         <input type="button" value="Modify User" onclick="requeueUser('${queue.queueId}')">
                     </td>
-                `;
-                    queueTable.appendChild(row);
-                });
-            }
+                    `;
+                            queueTable.appendChild(row);
+                        });
+                    }
+
+                    if(totalQueue) {
+                        totalQueue.innerHTML = queueCount;
+                    }
+
+                    if(totalMissed) {
+                        totalMissed.innerHTML = missedCount;
+                    }
+                    if(totalComplete) {
+                        totalComplete.innerHTML = dailyVolume;
+                    }
+                    if(totalReferred) {
+                        totalReferred.innerHTML = transferCount;
+                    }
+                })
+            fetch(`https://localhost:8443/queue/live-status?windowId=${windowId}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => {
+                    const currentServing = document.getElementById("currentServing");
+                    if (response.status === 204) {
+                        if(currentServing) {
+                            currentServing.innerHTML = "No one being served.";
+                        }
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(queue => {
+                    if(queue) {
+                        const currentServing = document.getElementById("currentServing");
+                        const currentVisited = document.getElementById("currentVisited");
+                        const currentPriority = document.getElementById("currentPriority");
+                        const currentMissed = document.getElementById("currentMissed");
+                        currentServingId = queue.queueId;
+                        if(currentServing) {
+                            currentServing.innerHTML = queue.user.name;
+                        }
+                        if(currentVisited) {
+                            currentVisited.innerHTML = queue.timeStamp;
+                        }
+                        if(currentPriority) {
+                            currentPriority.innerHTML = queue.user.priority;
+                        }
+                        if(currentMissed) {
+                            currentMissed.innerHTML = queue.callCount;
+                        }
+                    }
+                })
+
+            return mainFetch;
+
         })
         .catch(error => {
             console.error("An error occurred while loading the dashboard:", error);
         });
 }
+
 window.addEventListener("load", loadDashboard);
 
 function requeueUser(queueId) {
@@ -90,5 +173,34 @@ function requeueUser(queueId) {
         .catch(error => {
             console.error("Failed to re-queue client.", error);
             alert("Failed to re-queue client.");
+        })
+}
+
+function completeUser(currentServingId) {
+    fetch(`https://localhost:8443/queue/${currentServingId}`, {
+        method: "PUT",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            status: "COMPLETED",
+            priority: selectedStatus
+        })
+    })
+        .then(response => {
+            return response.json().then(body => {
+                if (!response.ok) {
+                    const err = new Error(body.error);
+                    err.code = body.code;
+                    throw err;
+                }
+                return body;
+            });
+        })
+        .then(complete => {
+            if(complete) {
+
+            }
         })
 }
