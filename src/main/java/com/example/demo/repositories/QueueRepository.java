065467b88c760f34468600bf30dbcc88b720a892
Wil-Ian <wilianmanuntag@gmail.com>
2026-07-13 09:08:04 +0800

    Updated queries to fit office requirements

--- src/main/java/com/example/demo/repositories/QueueRepository.java
+++ src/main/java/com/example/demo/repositories/QueueRepository.java
@@ -41,7 +41,7 @@ public interface QueueRepository extends JpaRepository<Queue, Integer> {
     @Query(value = "SELECT * FROM queue WHERE window_id = :windowId AND status IN ('COMPLETED', 'TRANSFERRED', 'NO_RESPONSE') ORDER BY time_stamp DESC", nativeQuery = true)
     List<Queue> finishedQueue(@Param("windowId") Integer windowId);
 
-    @Query(value = "SELECT * FROM queue WHERE status IN ('WAITING', 'TRANSFERRED') AND is_active = true ORDER BY time_stamp ASC", nativeQuery = true)
+    @Query(value = "SELECT q.* FROM queue q JOIN users u ON q.user_id = u.user_id WHERE status = 'WAITING' AND q.is_active = true ORDER BY u.priority ASC, time_stamp ASC", nativeQuery = true)
     List<Queue> findAllInQueue();
 
     @Query(value = "SELECT * FROM queue WHERE status = 'SERVING' AND is_active = true ORDER BY time_stamp ASC", nativeQuery = true)
