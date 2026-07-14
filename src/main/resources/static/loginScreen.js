018c0e16fccf4258ee778dc5cc2f675466b94bb1
Wil-Ian <wilianmanuntag@gmail.com>
2026-07-14 09:41:35 +0800

    Implemented loginSubmit listener with JWT storage

--- src/main/resources/static/loginScreen.js
+++ src/main/resources/static/loginScreen.js
@@ -6,9 +6,57 @@ document.getElementById("loginSubmit").addEventListener("click", function() {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
-        }
-    })
-        .then(r => {
-
+        },
+        body: JSON.stringify({
+            email: emailInput,
+            password: passwordInput
         })
-})
+    })
+        .then(response => {
+            return response.json().then(body => {
+                if (!response.ok) {
+                    throw new Error(body);
+                }
+                return body;
+            });
+        })
+        .then(success => {
+            localStorage.setItem('accessToken', success.accessToken);
+            localStorage.setItem('refreshToken', success.refreshToken);
+            window.location.href = "receivingDashboard.html";
+        })
+        .catch(error => {
+            console.error("Error.", error);
+            alert("Error.");
+        })
+
+});
+
+function updateDateTime() {
+    const now = new Date();
+    const options = {
+        weekday: 'long',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+    };
+    const dateElement = document.getElementById("current_time");
+    if (dateElement) {
+        dateElement.innerHTML = now.toLocaleString('en-US', options);
+    }
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
+
+function updateDate() {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    const dateElement = document.getElementById("current_date");
+    if (dateElement) {
+        dateElement.innerHTML = new Date().toLocaleDateString('en-US', options);
+    }
+}
+updateDate();
